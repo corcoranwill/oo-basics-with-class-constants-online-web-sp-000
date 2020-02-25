320a62ffbaef4d978ef0a2967eff01264d0bcb84
Will Corcoran <corcoranwill@gmail.com>
2020-02-25 01:25:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -13,4 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+  
 end
